b62832b3a25fba60ee170fc3cee97bc22f1b6777
Lukasz Niewinski <100769592+niewinski-l-pg@users.noreply.github.com>
2023-12-27 16:30:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'html/member.html',
+        scope: {
+            member: '='
+        }
+    }
+}git add member.js
